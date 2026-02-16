4ade889b4150f4dc15751d2d0fda0eae97166376
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-17 06:47:28 +0700

    Update interaction.js

--- src/interaction.js
+++ src/interaction.js
@@ -1,10 +1,18 @@
 export function initInteraction(camera) {
 
-  document.addEventListener('mousemove', (e) => {
-    const x = (e.clientX / window.innerWidth - 0.5) * 2
-    const y = (e.clientY / window.innerHeight - 0.5) * 2
+  let targetX = 0
+  let targetY = 0
 
-    camera.position.x = x * 3
-    camera.position.y = -y * 3
+  document.addEventListener('mousemove', (e) => {
+    targetX = (e.clientX / window.innerWidth - 0.5) * 6
+    targetY = (e.clientY / window.innerHeight - 0.5) * 6
   })
+
+  function animate() {
+    camera.position.x += (targetX - camera.position.x) * 0.05
+    camera.position.y += (-targetY - camera.position.y) * 0.05
+    requestAnimationFrame(animate)
+  }
+
+  animate()
 }
