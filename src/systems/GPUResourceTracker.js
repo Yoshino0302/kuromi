1718f02780289bbc5569d89e6fa05326294dc22e
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 14:44:03 +0700

    Update GPUResourceTracker.js

--- src/systems/GPUResourceTracker.js
+++ src/systems/GPUResourceTracker.js
@@ -1,43 +1,34 @@
-import * as THREE from 'https://jspm.dev/three'
 import { Logger } from '../utils/Logger.js'
 export class GPUResourceTracker{
 constructor(){
 this.resources=new Set()
-this.geometries=new Set()
-this.materials=new Set()
-this.textures=new Set()
-Logger.info('GPUResourceTracker created')}
+this.destroyed=false
+Logger.info('GPUResourceTracker constructed')}
 track(resource){
-if(!resource)return resource
-this.resources.add(resource)
-if(resource instanceof THREE.BufferGeometry)this.geometries.add(resource)
-else if(resource instanceof THREE.Material)this.materials.add(resource)
-else if(resource instanceof THREE.Texture)this.textures.add(resource)
-return resource}
+if(this.destroyed)return
+if(!resource)return
+if(this.resources.has(resource))return
+this.resources.add(resource)}
 untrack(resource){
+if(this.destroyed)return
 if(!resource)return
-this.resources.delete(resource)
-if(resource instanceof THREE.BufferGeometry)this.geometries.delete(resource)
-else if(resource instanceof THREE.Material)this.materials.delete(resource)
-else if(resource instanceof THREE.Texture)this.textures.delete(resource)}
-dispose(resource){
+if(!this.resources.has(resource))return
+this.resources.delete(resource)}
+getTrackedCount(){
+return this.resources.size}
+release(resource){
+if(this.destroyed)return
 if(!resource)return
-if(resource.dispose)resource.dispose()
-this.untrack(resource)}
-disposeAll(){
+if(this.resources.has(resource)){
+if(typeof resource.dispose==='function')resource.dispose()
+this.resources.delete(resource)}}
+releaseAll(){
+if(this.destroyed)return
 for(const resource of this.resources){
-if(resource.dispose)resource.dispose()}
-this.resources.clear()
-this.geometries.clear()
-this.materials.clear()
-this.textures.clear()
-Logger.info('GPU resources disposed')}
-getStats(){
-return{
-total:this.resources.size,
-geometries:this.geometries.size,
-materials:this.materials.size,
-textures:this.textures.size}}
-destroy(){
-this.disposeAll()
+if(resource&&typeof resource.dispose==='function')resource.dispose()}
+this.resources.clear()}
+async destroy(){
+if(this.destroyed)return
+this.releaseAll()
+this.destroyed=true
 Logger.info('GPUResourceTracker destroyed')}}
