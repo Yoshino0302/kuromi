259be064698cd80878e61f8b6de24aaf96bb2e10
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 14:41:32 +0700

    Update SceneManager.js

--- src/scenes/SceneManager.js
+++ src/scenes/SceneManager.js
@@ -1,46 +1,58 @@
 import { Logger } from '../utils/Logger.js'
 export class SceneManager{
 constructor(config={}){
+this.engine=config.engine||null
 this.renderer=config.renderer||null
 this.resourceManager=config.resourceManager||null
 this.gpuTracker=config.gpuTracker||null
 this.memoryTracker=config.memoryTracker||null
-this.activeScene=null
-this.activeSceneInstance=null
-this.sceneStack=[]
-Logger.info('SceneManager created')}
-async loadScene(SceneClass){
-if(!SceneClass)throw new Error('SceneClass required')
-if(this.activeSceneInstance)await this._unloadCurrentScene()
-const sceneInstance=new SceneClass({
-renderer:this.renderer,
-resourceManager:this.resourceManager,
-gpuTracker:this.gpuTracker,
-memoryTracker:this.memoryTracker})
-if(typeof sceneInstance.init==='function')await sceneInstance.init(sceneInstance.sceneWrapper)
-this.activeSceneInstance=sceneInstance
-this.activeScene=sceneInstance.getSceneWrapper()
-this.sceneStack.push(sceneInstance)
-Logger.info('Scene loaded: '+SceneClass.name)
-return sceneInstance}
-async switchScene(SceneClass){
-return this.loadScene(SceneClass)}
-async _unloadCurrentScene(){
-if(!this.activeSceneInstance)return
-if(typeof this.activeSceneInstance.destroy==='function')await this.activeSceneInstance.destroy()
-const index=this.sceneStack.indexOf(this.activeSceneInstance)
-if(index!==-1)this.sceneStack.splice(index,1)
-Logger.info('Scene unloaded: '+this.activeSceneInstance.constructor.name)
-this.activeSceneInstance=null
-this.activeScene=null}
+this.currentScene=null
+this.currentSceneWrapper=null
+this.loading=false
+this.destroyed=false
+Logger.info('SceneManager constructed')}
+async loadScene(SceneClass,config={}){
+if(this.destroyed)return
+if(this.loading)return
+this.loading=true
+const previousScene=this.currentScene
+try{
+if(previousScene){
+await previousScene.destroy()
+this.currentScene=null
+this.currentSceneWrapper=null}
+const sceneInstance=new SceneClass({renderer:this.renderer,resourceManager:this.resourceManager,gpuTracker:this.gpuTracker,memoryTracker:this.memoryTracker,...config})
+if(!sceneInstance)throw new Error('Scene instantiation failed')
+const sceneWrapper=sceneInstance.getSceneWrapper()
+if(!sceneWrapper||!sceneWrapper.scene||!sceneWrapper.camera||!sceneWrapper.renderer)throw new Error('Invalid sceneWrapper invariant')
+await sceneInstance.init(sceneWrapper)
+this.currentScene=sceneInstance
+this.currentSceneWrapper=sceneWrapper
+Logger.info('Scene loaded: '+SceneClass.name)}
+catch(err){
+Logger.error('Scene load failed',err)
+if(previousScene){
+this.currentScene=previousScene
+this.currentSceneWrapper=previousScene.getSceneWrapper()}}
+finally{
+this.loading=false}}
 update(delta){
-if(!this.activeSceneInstance)return
-if(typeof this.activeSceneInstance.update==='function')this.activeSceneInstance.update(delta)}
-getActiveScene(){
-return this.activeScene}
-getActiveSceneInstance(){
-return this.activeSceneInstance}
+if(this.destroyed)return
+if(!this.currentScene)return
+this.currentScene.update(delta)}
+resize(width,height){
+if(this.destroyed)return
+if(!this.currentScene)return
+this.currentScene.resize(width,height)}
+getSceneWrapper(){
+return this.currentSceneWrapper}
+getCurrentScene(){
+return this.currentScene}
 async destroy(){
-if(this.activeSceneInstance)await this._unloadCurrentScene()
-this.sceneStack.length=0
+if(this.destroyed)return
+if(this.currentScene){
+await this.currentScene.destroy()
+this.currentScene=null
+this.currentSceneWrapper=null}
+this.destroyed=true
 Logger.info('SceneManager destroyed')}}
