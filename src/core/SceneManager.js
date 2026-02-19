14d21fc93af8a3c81a403e0073d98d794c7aef07
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 12:12:49 +0700

    Update SceneManager.js

--- src/core/SceneManager.js
+++ src/core/SceneManager.js
@@ -1,61 +1,16 @@
-import * as THREE from 'https://jspm.dev/three'
-import { EffectComposer } from 'https://jspm.dev/three/examples/jsm/postprocessing/EffectComposer.js'
-import { RenderPass } from 'https://jspm.dev/three/examples/jsm/postprocessing/RenderPass.js'
-import { UnrealBloomPass } from 'https://jspm.dev/three/examples/jsm/postprocessing/UnrealBloomPass.js'
-
 export class SceneManager {
-
-  constructor(renderer, camera) {
-
-    this.renderer = renderer
-    this.camera = camera
-    this.currentScene = null
-    this.clock = new THREE.Clock()
-
-    this.composer = new EffectComposer(renderer)
-    this.renderPass = new RenderPass(new THREE.Scene(), camera)
-    this.composer.addPass(this.renderPass)
-
-    this.bloomPass = new UnrealBloomPass(
-  new THREE.Vector2(window.innerWidth, window.innerHeight),
-  1.2,   // strength
-  0.6,   // radius
-  0.4    // threshold
-)
-
-    this.composer.addPass(this.bloomPass)
-  }
-
-  setScene(sceneInstance) {
-
-    if (this.currentScene?.dispose) {
-      this.currentScene.dispose()
+    constructor() {
+        this.currentScene = null
     }
-
-    this.currentScene = sceneInstance
-
-    try {
-      this.currentScene.init()
-      this.renderPass.scene = this.currentScene.scene
-    } catch (e) {
-      console.error('Scene init failed:', e)
+    setScene(scene) {
+        if (this.currentScene) {
+            this.currentScene.dispose()
+        }
+        this.currentScene = scene
+        scene.init()
     }
-  }
-
-  update() {
-
-    if (!this.currentScene) return
-
-    const delta = this.clock.getDelta()
-
-    try {
-      if (this.currentScene.update) {
+    update(delta) {
+        if (!this.currentScene) return
         this.currentScene.update(delta)
-      }
-
-      this.composer.render()
-    } catch (e) {
-      console.error('Scene update error:', e)
     }
-  }
 }
