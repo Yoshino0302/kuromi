599b6f5b4e19d596620ddc1b6fca1215e72dc6b4
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 18:54:38 +0700

    Update CameraSystem.js

--- src/camera/CameraSystem.js
+++ src/camera/CameraSystem.js
@@ -1,6 +1,11 @@
+import * as THREE from 'https://jspm.dev/three'
+
 import { CinematicCamera } from './CinematicCamera.js'
 import { CameraShake } from './CameraShake.js'
 import { CameraController } from './CameraController.js'
+
+import { CinematicSequencer } from './sequencer/CinematicSequencer.js'
+import { CameraKeyframeTrack } from './sequencer/CameraKeyframeTrack.js'
 
 export class CameraSystem{
 
@@ -17,11 +22,55 @@ new CameraController(
 this.cinematicCamera.position
 )
 
+this.sequencer=
+new CinematicSequencer(
+this.cinematicCamera
+)
+
+this.createDemoSequence()
+
+}
+
+createDemoSequence(){
+
+const track=
+new CameraKeyframeTrack()
+
+track.addKeyframe(
+0,
+new THREE.Vector3(0,2,6),
+new THREE.Vector3(0,0,0)
+)
+
+track.addKeyframe(
+4,
+new THREE.Vector3(6,3,6),
+new THREE.Vector3(3,0,0)
+)
+
+track.addKeyframe(
+8,
+new THREE.Vector3(0,4,10),
+new THREE.Vector3(0,0,0)
+)
+
+track.addKeyframe(
+12,
+new THREE.Vector3(-6,3,6),
+new THREE.Vector3(-3,0,0)
+)
+
+this.sequencer.setTrack(track)
+
+this.sequencer.play()
+
 }
 
 update(delta){
 
 this.controller.update(delta)
+
+this.sequencer.update(delta)
 
 this.shake.update(delta)
 
