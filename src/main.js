65cb9db30d036e7b8ddd658df6575a5737ef6709
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 13:40:49 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { EngineCore } from './core/EngineCore.js'
 import { IntroScene } from './scenes/IntroScene.js'
+import { ValentineScene } from './scenes/ValentineScene.js'
 import { Logger } from './utils/Logger.js'
 const canvas=document.querySelector('canvas')||null
 const engine=new EngineCore({canvas})
@@ -7,6 +8,9 @@ async function boot(){
 try{
 await engine.init()
 await engine.loadScene(IntroScene)
+setTimeout(()=>{
+engine.loadScene(ValentineScene)
+},4000)
 engine.start()
 window.__ENGINE__=engine
 Logger.info('Engine boot complete')}
