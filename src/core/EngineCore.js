c0d81e5cb50930342e20335290e4a36dddbd162f
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 14:45:05 +0700

    Update EngineCore.js

--- src/core/EngineCore.js
+++ src/core/EngineCore.js
@@ -1,144 +1,48 @@
-import * as THREE from 'https://jspm.dev/three'
 import { Renderer } from '../renderer/Renderer.js'
 import { SceneManager } from '../scenes/SceneManager.js'
 import { ResourceManager } from '../systems/ResourceManager.js'
 import { GPUResourceTracker } from '../systems/GPUResourceTracker.js'
 import { MemoryTracker } from '../systems/MemoryTracker.js'
-import { UpdateScheduler } from '../systems/UpdateScheduler.js'
-import { Clock } from '../utils/Clock.js'
 import { Logger } from '../utils/Logger.js'
-export class EngineCore
-{
-    constructor(config = {})
-    {
-        this.config = config
-        this.initialized = false
-        this.running = false
-        this.destroyed = false
-        this.renderer = null
-        this.sceneManager = null
-        this.resourceManager = null
-        this.gpuTracker = null
-        this.memoryTracker = null
-        this.scheduler = null
-        this.clock = new Clock()
-        this._boundLoop = this._loop.bind(this)
-        Logger.info('EngineCore constructed')
-    }
-    async init()
-    {
-        if (this.initialized)
-        {
-            Logger.warn('Engine already initialized')
-            return
-        }
-        Logger.info('Engine initialization started')
-        this._initSystems()
-        this._initRenderer()
-        this._initSceneManager()
-        this.initialized = true
-        Logger.info('Engine initialization completed')
-    }
-    _initSystems()
-    {
-        this.memoryTracker = new MemoryTracker()
-        this.gpuTracker = new GPUResourceTracker()
-        this.resourceManager = new ResourceManager({
-            gpuTracker: this.gpuTracker,
-            memoryTracker: this.memoryTracker
-        })
-        this.scheduler = new UpdateScheduler()
-        Logger.info('Systems initialized')
-    }
-    _initRenderer()
-    {
-        this.renderer = new Renderer({
-            canvas: this.config.canvas,
-            gpuTracker: this.gpuTracker
-        })
-        Logger.info('Renderer initialized')
-    }
-    _initSceneManager()
-    {
-        this.sceneManager = new SceneManager({
-            renderer: this.renderer,
-            resourceManager: this.resourceManager,
-            gpuTracker: this.gpuTracker,
-            memoryTracker: this.memoryTracker
-        })
-        Logger.info('SceneManager initialized')
-    }
-    start()
-    {
-        if (!this.initialized)
-        {
-            throw new Error('Engine must be initialized before start')
-        }
-        if (this.running)
-        {
-            Logger.warn('Engine already running')
-            return
-        }
-        this.running = true
-        this.clock.start()
-        requestAnimationFrame(this._boundLoop)
-        Logger.info('Engine started')
-    }
-    stop()
-    {
-        this.running = false
-        Logger.info('Engine stopped')
-    }
-    async destroy()
-    {
-        if (this.destroyed)
-        {
-            return
-        }
-        this.stop()
-        await this.sceneManager.destroy()
-        this.resourceManager.destroy()
-        this.renderer.destroy()
-        this.gpuTracker.destroy()
-        this.memoryTracker.destroy()
-        this.scheduler.destroy()
-        this.destroyed = true
-        Logger.info('Engine destroyed')
-    }
-    _loop()
-    {
-        if (!this.running)
-        {
-            return
-        }
-        const delta = this.clock.getDelta()
-        this.scheduler.update(delta)
-        this.sceneManager.update(delta)
-        this.renderer.render(this.sceneManager.getActiveScene())
-        requestAnimationFrame(this._boundLoop)
-    }
-    async loadScene(sceneClass)
-    {
-        return this.sceneManager.loadScene(sceneClass)
-    }
-    getRenderer()
-    {
-        return this.renderer
-    }
-    getSceneManager()
-    {
-        return this.sceneManager
-    }
-    getResourceManager()
-    {
-        return this.resourceManager
-    }
-    getGPUTracker()
-    {
-        return this.gpuTracker
-    }
-    getMemoryTracker()
-    {
-        return this.memoryTracker
-    }
-}
+export class EngineCore{
+constructor(config={}){
+this.canvas=config.canvas||document.querySelector('canvas')
+this.renderer=new Renderer({canvas:this.canvas})
+this.gpuTracker=new GPUResourceTracker()
+this.memoryTracker=new MemoryTracker()
+this.resourceManager=new ResourceManager({gpuTracker:this.gpuTracker})
+this.sceneManager=new SceneManager({engine:this,renderer:this.renderer.getThreeRenderer(),resourceManager:this.resourceManager,gpuTracker:this.gpuTracker,memoryTracker:this.memoryTracker})
+this.running=false
+this.destroyed=false
+this._boundLoop=this._loop.bind(this)
+Logger.info('EngineCore constructed')}
+async loadScene(SceneClass,config={}){
+if(this.destroyed)return
+await this.sceneManager.loadScene(SceneClass,config)}
+start(){
+if(this.destroyed)return
+if(this.running)return
+this.running=true
+requestAnimationFrame(this._boundLoop)
+Logger.info('EngineCore started')}
+_loop(time){
+if(!this.running||this.destroyed)return
+const delta=time*0.001
+this.sceneManager.update(delta)
+const sceneWrapper=this.sceneManager.getSceneWrapper()
+if(sceneWrapper)this.renderer.render(sceneWrapper)
+requestAnimationFrame(this._boundLoop)}
+resize(width,height){
+if(this.destroyed)return
+this.renderer.setSize(width,height)
+this.sceneManager.resize(width,height)}
+async destroy(){
+if(this.destroyed)return
+this.running=false
+await this.sceneManager.destroy()
+await this.renderer.destroy()
+await this.resourceManager.destroy()
+await this.gpuTracker.destroy()
+if(this.memoryTracker&&typeof this.memoryTracker.destroy==='function')await this.memoryTracker.destroy()
+this.destroyed=true
+Logger.info('EngineCore destroyed')}}
