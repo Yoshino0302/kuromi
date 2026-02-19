4a0748db2770b24ea3e6afbcbd255b19fc8e9e17
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 20:26:03 +0700

    Update Engine.js

--- src/core/Engine.js
+++ src/core/Engine.js
@@ -1,11 +1,15 @@
+import * as THREE from 'https://jspm.dev/three'
 import { Renderer } from '../renderer/Renderer.js'
 import { SceneManager } from '../scene/SceneManager.js'
 import { CameraSystem } from '../camera/CameraSystem.js'
 import { PerformanceMonitor } from '../systems/PerformanceMonitor.js'
+import { PerformanceScaler } from '../systems/PerformanceScaler.js'
 
 export class Engine{
 
 constructor(){
+
+this.clock=new THREE.Clock()
 
 this.renderer=new Renderer()
 
@@ -15,48 +19,77 @@ this.cameraSystem=new CameraSystem()
 
 this.performanceMonitor=new PerformanceMonitor()
 
-this.lastTime=performance.now()
+this.performanceScaler=new PerformanceScaler(
+this.renderer.getRenderer()
+)
 
 this.running=false
 
-}
-
-start(){
-
-this.running=true
-
-this.loop()
+this.bindResize()
 
 }
 
-stop(){
+bindResize(){
 
-this.running=false
+window.addEventListener(
+'resize',
+()=>{
+this.renderer.resize()
+}
+)
 
 }
 
-loop(){
-
-if(!this.running)return
-
-const now=performance.now()
-
-const delta=(now-this.lastTime)/1000
-
-this.lastTime=now
-
-this.performanceMonitor.update(delta)
+update(delta){
 
 this.sceneManager.update(delta)
 
 this.cameraSystem.update(delta)
+
+const fps=
+this.performanceMonitor.update(delta)
+
+this.performanceScaler.update(fps)
+
+}
+
+render(){
 
 this.renderer.render(
 this.sceneManager.getScene(),
 this.cameraSystem.getCamera()
 )
 
-requestAnimationFrame(()=>this.loop())
+}
+
+start(){
+
+if(this.running)return
+
+this.running=true
+
+const loop=()=>{
+
+if(!this.running)return
+
+const delta=
+this.clock.getDelta()
+
+this.update(delta)
+
+this.render()
+
+requestAnimationFrame(loop)
+
+}
+
+loop()
+
+}
+
+stop(){
+
+this.running=false
 
 }
 
