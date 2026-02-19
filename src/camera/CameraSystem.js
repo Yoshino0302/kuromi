15f7252ef76d5497cef27285ff8153853fbef70d
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 18:48:08 +0700

    Update CameraSystem.js

--- src/camera/CameraSystem.js
+++ src/camera/CameraSystem.js
@@ -1,45 +1,47 @@
-import * as THREE from 'https://jspm.dev/three'
+import { CinematicCamera } from './CinematicCamera.js'
+import { CameraShake } from './CameraShake.js'
+import { CameraController } from './CameraController.js'
 
 export class CameraSystem{
 
 constructor(){
 
-this.camera=new THREE.PerspectiveCamera(
-60,
-window.innerWidth/window.innerHeight,
-0.1,
-1000
+this.cinematicCamera=
+new CinematicCamera()
+
+this.shake=
+new CameraShake()
+
+this.controller=
+new CameraController(
+this.cinematicCamera.position
 )
-
-this.camera.position.set(0,0,5)
-
-this.target=new THREE.Vector3(0,0,0)
-
-window.addEventListener(
-'resize',
-()=>this.onResize()
-)
-
-}
-
-onResize(){
-
-this.camera.aspect=
-window.innerWidth/window.innerHeight
-
-this.camera.updateProjectionMatrix()
 
 }
 
 update(delta){
 
-this.camera.lookAt(this.target)
+this.controller.update(delta)
+
+this.shake.update(delta)
+
+this.cinematicCamera.addShake(
+this.shake.getOffset()
+)
+
+this.cinematicCamera.update(delta)
 
 }
 
 getCamera(){
 
-return this.camera
+return this.cinematicCamera.getCamera()
+
+}
+
+shakeCamera(intensity){
+
+this.shake.trigger(intensity)
 
 }
 
