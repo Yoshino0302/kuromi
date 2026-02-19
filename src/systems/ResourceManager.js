46fe3ba8143d7b1577de6e6577206add38b1959a
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 14:43:07 +0700

    Update ResourceManager.js

--- src/systems/ResourceManager.js
+++ src/systems/ResourceManager.js
@@ -2,60 +2,50 @@ import * as THREE from 'https://jspm.dev/three'
 import { Logger } from '../utils/Logger.js'
 export class ResourceManager{
 constructor(config={}){
-this.gpuTracker=config.gpuTracker||null
-this.memoryTracker=config.memoryTracker||null
 this.textureLoader=new THREE.TextureLoader()
 this.cache=new Map()
 this.loading=new Map()
-Logger.info('ResourceManager created')}
+this.gpuTracker=config.gpuTracker||null
+this.destroyed=false
+Logger.info('ResourceManager constructed')}
 async loadTexture(url){
+if(this.destroyed)throw new Error('ResourceManager destroyed')
 if(this.cache.has(url))return this.cache.get(url)
 if(this.loading.has(url))return this.loading.get(url)
 const promise=new Promise((resolve,reject)=>{
 this.textureLoader.load(url,(texture)=>{
+if(this.destroyed){
+texture.dispose()
+reject(new Error('ResourceManager destroyed during load'))
+return}
+texture.wrapS=THREE.RepeatWrapping
+texture.wrapT=THREE.RepeatWrapping
 texture.colorSpace=THREE.SRGBColorSpace
-texture.generateMipmaps=true
-texture.minFilter=THREE.LinearMipmapLinearFilter
-texture.magFilter=THREE.LinearFilter
-texture.wrapS=THREE.ClampToEdgeWrapping
-texture.wrapT=THREE.ClampToEdgeWrapping
 this.cache.set(url,texture)
 this.loading.delete(url)
 if(this.gpuTracker)this.gpuTracker.track(texture)
-if(this.memoryTracker)this.memoryTracker.track(texture)
-Logger.info('Texture loaded: '+url)
 resolve(texture)},undefined,(err)=>{
 this.loading.delete(url)
-Logger.error('Texture failed: '+url)
+Logger.error('Texture load failed: '+url,err)
 reject(err)})})
 this.loading.set(url,promise)
 return promise}
-get(url){
+getTexture(url){
 return this.cache.get(url)||null}
-has(url){
+hasTexture(url){
 return this.cache.has(url)}
-unload(url){
+unloadTexture(url){
 if(!this.cache.has(url))return
-const resource=this.cache.get(url)
-if(resource.dispose)resource.dispose()
-if(this.gpuTracker)this.gpuTracker.untrack(resource)
-if(this.memoryTracker)this.memoryTracker.untrack(resource)
-this.cache.delete(url)
-Logger.info('Resource unloaded: '+url)}
-unloadAll(){
-for(const [url,resource]of this.cache){
-if(resource.dispose)resource.dispose()
-if(this.gpuTracker)this.gpuTracker.untrack(resource)
-if(this.memoryTracker)this.memoryTracker.untrack(resource)
-Logger.info('Resource unloaded: '+url)}
+const texture=this.cache.get(url)
+if(this.gpuTracker)this.gpuTracker.untrack(texture)
+texture.dispose()
+this.cache.delete(url)}
+async destroy(){
+if(this.destroyed)return
+for(const texture of this.cache.values()){
+if(this.gpuTracker)this.gpuTracker.untrack(texture)
+texture.dispose()}
 this.cache.clear()
-this.loading.clear()}
-destroy(){
-this.unloadAll()
+this.loading.clear()
+this.destroyed=true
 Logger.info('ResourceManager destroyed')}}
-destroy(){
- for(const texture of this.cache.values()){
-  texture.dispose()
- }
- this.cache.clear()
-}
