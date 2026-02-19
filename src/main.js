c460a585089d83319bca5ef48470b09987d7a4cf
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 13:31:42 +0700

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { EngineCore } from './core/EngineCore.js'
+import { IntroScene } from './scenes/IntroScene.js'
+import { Logger } from './utils/Logger.js'
+const canvas=document.querySelector('canvas')||null
+const engine=new EngineCore({canvas})
+async function boot(){
+try{
+await engine.init()
+await engine.loadScene(IntroScene)
+engine.start()
+window.__ENGINE__=engine
+Logger.info('Engine boot complete')}
+catch(e){
+Logger.error('Engine boot failed',e)}}
+boot()
+export { engine }
