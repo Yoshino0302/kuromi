1c25cc58151a03345a6038fb9634ee3184987bc5
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 12:12:13 +0700

    Update Renderer.js

--- src/renderer/Renderer.js
+++ src/renderer/Renderer.js
@@ -1,1 +1,30 @@
-
+import * as THREE from 'https://jspm.dev/three'
+export class Renderer {
+    constructor(canvas) {
+        this.renderer = new THREE.WebGLRenderer({
+            canvas,
+            antialias: true,
+            powerPreference: "high-performance"
+        })
+        this.renderer.outputColorSpace = THREE.SRGBColorSpace
+        this.renderer.toneMapping = THREE.ACESFilmicToneMapping
+        this.renderer.toneMappingExposure = 1.25
+        this.renderer.setPixelRatio(
+            Math.min(window.devicePixelRatio, 2)
+        )
+        this.resize()
+        window.addEventListener(
+            'resize',
+            () => this.resize()
+        )
+    }
+    resize() {
+        this.renderer.setSize(
+            window.innerWidth,
+            window.innerHeight
+        )
+    }
+    render(scene, camera) {
+        this.renderer.render(scene, camera)
+    }
+}
