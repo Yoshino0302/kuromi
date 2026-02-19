039dd40431db0eba0f57489eb4983c71b1823db8
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 12:10:30 +0700

    Update Disposable.js

--- src/utils/Disposable.js
+++ src/utils/Disposable.js
@@ -1,31 +1,16 @@
-
 export class Disposable {
-
     constructor() {
-
         this._disposed = false
-
     }
-
     get disposed() {
-
         return this._disposed
-
     }
-
     dispose() {
-
         if (this._disposed) return
-
         this._disposed = true
-
         this.onDispose()
-
     }
-
     onDispose() {
         // override in subclasses
     }
-
 }
-
