6375fd676b9bb7045c9566abe6b0ef050fe56c07
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 18:39:40 +0700

    Update SceneManager.js

--- src/scenes/SceneManager.js
+++ src/scenes/SceneManager.js
@@ -1,1 +1,75 @@
-export class SceneManager{constructor(engine){this.engine=engine;this.current=null}set(scene){if(this.current)this.current.dispose();this.current=scene;scene.init()}update(dt){if(this.current)this.current.update(dt)}}
+import * as THREE from 'https://jspm.dev/three'
+
+export class SceneManager{
+
+constructor(){
+
+this.scene=new THREE.Scene()
+
+this.scene.background=new THREE.Color(0x000000)
+
+this.createTestObject()
+
+this.createLights()
+
+}
+
+createTestObject(){
+
+const geometry=new THREE.BoxGeometry(1,1,1)
+
+const material=new THREE.MeshStandardMaterial({
+color:0xff00ff,
+emissive:0x220022,
+roughness:0.3,
+metalness:0.8
+})
+
+this.mesh=new THREE.Mesh(
+geometry,
+material
+)
+
+this.scene.add(this.mesh)
+
+}
+
+createLights(){
+
+const light=new THREE.DirectionalLight(
+0xffffff,
+3
+)
+
+light.position.set(5,5,5)
+
+this.scene.add(light)
+
+const ambient=new THREE.AmbientLight(
+0xffffff,
+0.3
+)
+
+this.scene.add(ambient)
+
+}
+
+update(delta){
+
+if(this.mesh){
+
+this.mesh.rotation.x+=delta*0.5
+
+this.mesh.rotation.y+=delta*0.7
+
+}
+
+}
+
+getScene(){
+
+return this.scene
+
+}
+
+}
