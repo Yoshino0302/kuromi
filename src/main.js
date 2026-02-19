a31e772d45c46a365a30b2618c97a1a1c9aed21b
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-19 14:12:45 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
+/*
+FILE: src/main.js
+ROLE: Engine entry point compatible with EngineCore API
+*/
 import { EngineCore } from './core/EngineCore.js'
 import { ValentineScene } from './scenes/ValentineScene.js'
 import { Logger } from './utils/Logger.js'
 class Application{
 constructor(){
 this.engine=null
-this.scene=null
 this._boot()}
-_boot(){
+async _boot(){
 Logger.info('Application boot')
 this.engine=new EngineCore({
 containerId:'app',
 debug:true})
-this.scene=new ValentineScene(this.engine)
-this.engine.sceneManager.setScene(this.scene)
+await this.engine.loadScene(ValentineScene)
 this.engine.start()
 Logger.info('Valentine cinematic experience started')}}
 window.addEventListener('DOMContentLoaded',()=>{
