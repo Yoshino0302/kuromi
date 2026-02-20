7553c29a48a4d643c90c13407bf693799d977850
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-20 21:08:33 +0700

    Update Engine.js

--- src/core/Engine.js
+++ src/core/Engine.js
@@ -35,10 +35,7 @@ return Engine.instance
 
 constructor(options={}){
 
-if(Engine.instance){
-return Engine.instance
-}
-
+if(Engine.instance)return Engine.instance
 Engine.instance=this
 
 this.options=options||{}
@@ -51,6 +48,7 @@ this.running=false
 this.initialized=false
 this.destroyed=false
 this.paused=false
+this._disposed=false
 
 this.clock=new THREE.Clock(false)
 
@@ -59,17 +57,18 @@ this.time=0
 this.frame=0
 
 this.timeScale=1
+
 this.fixedDelta=1/60
 this.accumulator=0
 this.alpha=0
 this.maxSubSteps=10
 
-this.deterministic=false
-this.lockStep=false
-this.fixedSeed=0
-
-this.maxDelta=0.05
+this.maxDelta=0.25
 this.minDelta=0.000001
+
+this.deterministic=options.deterministic===true
+this.lockStep=options.lockStep===true
+this.fixedSeed=options.fixedSeed??0
 
 this.renderer=null
 this.pipeline=null
@@ -94,11 +93,12 @@ this._disabledSystems=new WeakSet()
 
 this._resizeObserver=null
 
+this._lastNow=0
+this._panic=false
+
 this._boundTick=this._tick.bind(this)
 this._boundResize=this._handleResize.bind(this)
 this._boundVisibility=this._handleVisibility.bind(this)
-
-this._lastNow=0
 
 Object.seal(this)
 
@@ -112,8 +112,11 @@ if(this._initPromise)return this._initPromise
 this._initPromise=(async()=>{
 
 this.state=ENGINE_STATE.INITIALIZING
-
 this._emit('init:start')
+
+if(this.deterministic){
+this._initDeterministicSeed(this.fixedSeed)
+}
 
 this.renderer=new Renderer({...this.options,engine:this})
 this.pipeline=new RenderPipeline(this)
@@ -166,6 +169,7 @@ this._installVisibility()
 
 this.initialized=true
 this.destroyed=false
+this._disposed=false
 
 this.state=ENGINE_STATE.INITIALIZED
 
@@ -179,9 +183,22 @@ return this._initPromise
 
 }
 
+_initDeterministicSeed(seed){
+
+let s=seed||1
+
+Math.random=(()=>{
+return()=>{
+s=(s*16807)%2147483647
+return(s-1)/2147483646
+}
+})()
+
+}
+
 async start(){
 
-if(this.destroyed)return
+if(this.destroyed||this._disposed)return
 if(this.running)return
 
 if(!this.initialized){
@@ -223,6 +240,7 @@ if(!this.running)return
 if(!this.paused)return
 
 this.paused=false
+
 this.clock.start()
 
 this._lastNow=performance.now()
@@ -281,13 +299,24 @@ this._loopActive=false
 
 _tick(now){
 
-let delta=(now-this._lastNow)*0.001
+let delta
+
+if(this.lockStep){
+delta=this.fixedDelta
+}else{
+delta=(now-this._lastNow)*0.001
+}
 
 this._lastNow=now
 
 if(!Number.isFinite(delta))delta=0
 
-if(delta>this.maxDelta)delta=this.maxDelta
+if(delta>this.maxDelta){
+delta=this.maxDelta
+this._panic=true
+this._emit('panic',delta)
+}
+
 if(delta<this.minDelta)delta=this.minDelta
 
 delta*=this.timeScale
@@ -302,9 +331,23 @@ let subSteps=0
 
 this._emit('frame:start',delta)
 
-while(this.accumulator>=this.fixedDelta&&subSteps<this.maxSubSteps){
+while(this.accumulator>=this.fixedDelta){
 
-this._fixedUpdate(this.fixedDelta)
+if(subSteps>=this.maxSubSteps){
+
+this.accumulator=0
+this._panic=true
+
+this._emit('panic:spiral')
+
+break
+
+}
+
+this._emit('frame:fixed',this.fixedDelta)
+
+this._safeCall(this.scheduler,'fixedUpdate',this.fixedDelta)
+this._safeCall(this.systemManager,'fixedUpdate',this.fixedDelta)
 
 this.accumulator-=this.fixedDelta
 
@@ -314,43 +357,30 @@ subSteps++
 
 this.alpha=this.accumulator/this.fixedDelta
 
-this._update(delta)
-this._lateUpdate(delta)
-
-const fps=this.performanceMonitor?this._safeCall(this.performanceMonitor,'update',delta):0
-
-this.performanceScaler&&this._safeCall(this.performanceScaler,'update',fps,delta)
-
-this.memoryMonitor&&this._safeCall(this.memoryMonitor,'update',delta)
-
 this._emit('frame:update',delta)
+
+this._safeCall(this.scheduler,'update',delta)
+this._safeCall(this.systemManager,'update',delta)
+this._safeCall(this.environmentSystem,'update',delta)
+this._safeCall(this.cameraSystem,'update',delta)
+this._safeCall(this.sceneManager,'update',delta,this.time,this.alpha)
+
+this._emit('frame:late',delta)
+
+this._safeCall(this.systemManager,'lateUpdate',delta)
+
+const fps=this._safeCall(this.performanceMonitor,'update',delta)||0
+
+this._safeCall(this.performanceScaler,'update',fps,delta)
+this._safeCall(this.memoryMonitor,'update',delta)
+
+this._emit('frame:render',delta)
 
 this._render(delta,this.alpha)
 
 this._emit('frame:end',delta)
 
-}
-
-_fixedUpdate(delta){
-
-this.scheduler&&this._safeCall(this.scheduler,'fixedUpdate',delta)
-this.systemManager&&this._safeCall(this.systemManager,'fixedUpdate',delta)
-
-}
-
-_update(delta){
-
-this.scheduler&&this._safeCall(this.scheduler,'update',delta)
-this.systemManager&&this._safeCall(this.systemManager,'update',delta)
-this.environmentSystem&&this._safeCall(this.environmentSystem,'update',delta)
-this.cameraSystem&&this._safeCall(this.cameraSystem,'update',delta)
-this.sceneManager&&this._safeCall(this.sceneManager,'update',delta,this.time)
-
-}
-
-_lateUpdate(delta){
-
-this.systemManager&&this._safeCall(this.systemManager,'lateUpdate',delta)
+this._panic=false
 
 }
 
@@ -363,13 +393,9 @@ const camera=this.cameraSystem?.getCamera?.()
 if(!renderer||!scene||!camera)return
 
 if(this.pipeline?.render){
-
 this._safeCall(this.pipeline,'render',renderer,scene,camera,delta,alpha)
-
 }else{
-
 this._safeCall(renderer,'render',scene,camera)
-
 }
 
 }
@@ -377,7 +403,6 @@ this._safeCall(renderer,'render',scene,camera)
 _safeCall(obj,method,...args){
 
 if(!obj)return
-
 if(this._disabledSystems.has(obj))return
 
 const fn=obj?.[method]
@@ -390,30 +415,13 @@ return fn.apply(obj,args)
 
 this._disabledSystems.add(obj)
 
+this._emit('system:error',{system:obj,error:e})
+
 if(this.debug){
 console.warn('[ENGINE SYSTEM DISABLED]',obj,e)
 }
 
-this._emit('system:error',{system:obj,error:e})
-
 }
-
-}
-
-stop(){
-
-if(!this.running)return
-
-this.running=false
-this.paused=false
-
-this.clock.stop()
-
-this._stopLoop()
-
-this.state=ENGINE_STATE.STOPPED
-
-this._emit('stop')
 
 }
 
@@ -446,6 +454,9 @@ await this._dispose(this.performanceMonitor)
 
 this.initialized=false
 this.destroyed=true
+this._disposed=true
+
+Engine.instance=null
 
 this.state=ENGINE_STATE.DESTROYED
 
@@ -457,14 +468,31 @@ return this._shutdownPromise
 
 }
 
+stop(){
+
+if(!this.running)return
+
+this.running=false
+this.paused=false
+
+this.clock.stop()
+
+this._stopLoop()
+
+this.state=ENGINE_STATE.STOPPED
+
+this._emit('stop')
+
+}
+
 async restart(){
 
 await this.shutdown()
 
 this.destroyed=false
+this._disposed=false
 
 await this.init()
-
 await this.start()
 
 }
@@ -492,12 +520,7 @@ this.renderer?.resize?.()
 const size=this.renderer?.getSize?.()
 
 if(size){
-
-this.performanceScaler?.setSize?.(
-size.width,
-size.height
-)
-
+this.performanceScaler?.setSize?.(size.width,size.height)
 }
 
 this._emit('resize')
@@ -513,7 +536,6 @@ const canvas=this.renderer?.getCanvas?.()
 if(canvas&&typeof ResizeObserver!=='undefined'){
 
 this._resizeObserver=new ResizeObserver(this._boundResize)
-
 this._resizeObserver.observe(canvas)
 
 }
@@ -604,5 +626,6 @@ isRunning(){return this.running}
 isPaused(){return this.paused}
 isInitialized(){return this.initialized}
 isDestroyed(){return this.destroyed}
+isDisposed(){return this._disposed}
 
 }
