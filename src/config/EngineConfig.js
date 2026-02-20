37099613dc1dcab3000d8b0adb8dcf05ca4831a9
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-20 10:55:37 +0700

    Update EngineConfig.js

--- src/config/EngineConfig.js
+++ src/config/EngineConfig.js
@@ -1,4 +1,4 @@
-export const EngineConfig={
+const DEFAULTS={
 
 version:'1.0.0',
 
@@ -91,17 +91,104 @@ disposeGuard:true
 
 }
 
-export function createEngineConfig(overrides={}){
+export class EngineConfig{
 
-const config=structuredClone(EngineConfig)
+constructor(overrides={}){
 
-merge(config,overrides)
+this._config=_deepMerge(
+structuredClone(DEFAULTS),
+overrides
+)
 
-return config
+this._validate()
+
+Object.freeze(this._config)
+Object.freeze(this)
 
 }
 
-function merge(target,source){
+_validate(){
+
+const c=this._config
+
+if(c.fixedDelta<=0)throw new Error('EngineConfig.fixedDelta must be > 0')
+
+if(c.maxDelta<=0)throw new Error('EngineConfig.maxDelta must be > 0')
+
+if(c.minDelta<=0)throw new Error('EngineConfig.minDelta must be > 0')
+
+if(c.timeScale<=0)throw new Error('EngineConfig.timeScale must be > 0')
+
+if(c.pixelRatio.min<=0)throw new Error('EngineConfig.pixelRatio.min must be > 0')
+
+if(c.pixelRatio.max<=0)throw new Error('EngineConfig.pixelRatio.max must be > 0')
+
+if(c.pixelRatio.max<c.pixelRatio.min)throw new Error('EngineConfig.pixelRatio.max must be >= min')
+
+if(c.performance.targetFPS<=0)throw new Error('EngineConfig.performance.targetFPS must be > 0')
+
+if(c.performance.minFPS<=0)throw new Error('EngineConfig.performance.minFPS must be > 0')
+
+}
+
+get version(){return this._config.version}
+get name(){return this._config.name}
+get environment(){return this._config.environment}
+get debug(){return this._config.debug}
+get autoStart(){return this._config.autoStart}
+get fixedTimeStep(){return this._config.fixedTimeStep}
+get fixedDelta(){return this._config.fixedDelta}
+get maxDelta(){return this._config.maxDelta}
+get minDelta(){return this._config.minDelta}
+get timeScale(){return this._config.timeScale}
+get pauseOnHidden(){return this._config.pauseOnHidden}
+get resumeOnVisible(){return this._config.resumeOnVisible}
+get container(){return this._config.container}
+get canvas(){return this._config.canvas}
+get resolution(){return this._config.resolution}
+get pixelRatio(){return this._config.pixelRatio}
+get performance(){return this._config.performance}
+get lifecycle(){return this._config.lifecycle}
+get systems(){return this._config.systems}
+get renderer(){return this._config.renderer}
+get input(){return this._config.input}
+get animation(){return this._config.animation}
+get memory(){return this._config.memory}
+get events(){return this._config.events}
+get safety(){return this._config.safety}
+
+toJSON(){
+
+return structuredClone(this._config)
+
+}
+
+merge(overrides={}){
+
+return new EngineConfig(
+_deepMerge(
+structuredClone(this._config),
+overrides
+)
+)
+
+}
+
+static create(overrides={}){
+
+return new EngineConfig(overrides)
+
+}
+
+static getDefaults(){
+
+return structuredClone(DEFAULTS)
+
+}
+
+}
+
+function _deepMerge(target,source){
 
 for(const key in source){
 
@@ -115,7 +202,7 @@ typeof value==='object'&&
 
 if(!target[key])target[key]={}
 
-merge(target[key],value)
+_deepMerge(target[key],value)
 
 }else{
 
@@ -124,5 +211,7 @@ target[key]=value
 }
 
 }
+
+return target
 
 }
