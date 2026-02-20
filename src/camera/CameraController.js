cb7fe2e02ac6462110826c4db31af23d539cf4af
Yoshino0302 <85216923+Yoshino0302@users.noreply.github.com>
2026-02-20 07:44:48 +0700

    Update CameraController.js

--- src/camera/CameraController.js
+++ src/camera/CameraController.js
@@ -1,19 +1,33 @@
 import * as THREE from 'https://jspm.dev/three'
 
+const CONTROLLER_STATE={
+INITIALIZED:0,
+ACTIVE:1,
+DISABLED:2,
+DISPOSED:3
+}
+
 export class CameraController{
 
 constructor(camera,options={}){
 
 this.camera=camera
-
 this.options=options
+
+this.state=CONTROLLER_STATE.INITIALIZED
+this.disposed=false
 
 this.enabled=true
 
-this.moveSpeed=options.moveSpeed??6.0
-this.acceleration=options.acceleration??14.0
-this.deceleration=options.deceleration??10.0
-this.maxVelocity=options.maxVelocity??10.0
+this.moveSpeed=options.moveSpeed??6
+this.sprintMultiplier=options.sprintMultiplier??2
+
+this.acceleration=options.acceleration??16
+this.deceleration=options.deceleration??14
+
+this.maxVelocity=options.maxVelocity??12
+
+this.space=options.space??'camera'
 
 this.velocity=new THREE.Vector3()
 this.direction=new THREE.Vector3()
@@ -21,6 +35,8 @@ this.direction=new THREE.Vector3()
 this.forward=new THREE.Vector3()
 this.right=new THREE.Vector3()
 this.up=new THREE.Vector3(0,1,0)
+
+this.moveVector=new THREE.Vector3()
 
 this.keys=Object.create(null)
 
@@ -37,82 +53,128 @@ this.keys[e.code]=false
 }
 
 window.addEventListener('keydown',this._keydownHandler,{passive:true})
-
 window.addEventListener('keyup',this._keyupHandler,{passive:true})
-
-this.state='initialized'
-
-this.disposed=false
 
 }
 
 update(delta){
 
 if(this.disposed)return
-
 if(!this.enabled)return
-
 if(!this.camera)return
+if(delta<=0)return
 
-this._computeDirection()
+this._computeInputDirection()
 
-this._integrateVelocity(delta)
+this._updateVelocity(delta)
 
 this._applyMovement(delta)
 
 }
 
-_computeDirection(){
+_computeInputDirection(){
 
-this.direction.set(0,0,0)
+const dir=this.direction
 
-if(this.keys['KeyW'])this.direction.z-=1
-if(this.keys['KeyS'])this.direction.z+=1
-if(this.keys['KeyA'])this.direction.x-=1
-if(this.keys['KeyD'])this.direction.x+=1
-if(this.keys['Space'])this.direction.y+=1
-if(this.keys['ShiftLeft'])this.direction.y-=1
+dir.set(0,0,0)
 
-if(this.direction.lengthSq()>0){
+if(this.keys['KeyW'])dir.z-=1
+if(this.keys['KeyS'])dir.z+=1
+if(this.keys['KeyA'])dir.x-=1
+if(this.keys['KeyD'])dir.x+=1
+if(this.keys['Space'])dir.y+=1
+if(this.keys['ShiftLeft'])dir.y-=1
 
-this.direction.normalize()
+if(dir.lengthSq()>1){
+
+dir.normalize()
 
 }
 
 }
 
-_integrateVelocity(delta){
+_updateVelocity(delta){
 
-if(this.direction.lengthSq()>0){
+const vel=this.velocity
+const dir=this.direction
 
-this.forward.set(0,0,-1).applyQuaternion(this.camera.quaternion)
-this.right.set(1,0,0).applyQuaternion(this.camera.quaternion)
+if(dir.lengthSq()>0){
 
-const moveVec=new THREE.Vector3()
+this._computeMovementBasis()
 
-moveVec.addScaledVector(this.forward,this.direction.z)
-moveVec.addScaledVector(this.right,this.direction.x)
-moveVec.addScaledVector(this.up,this.direction.y)
+this.moveVector.set(0,0,0)
 
-moveVec.normalize()
+this.moveVector.addScaledVector(this.forward,dir.z)
+this.moveVector.addScaledVector(this.right,dir.x)
+this.moveVector.addScaledVector(this.up,dir.y)
+
+if(this.moveVector.lengthSq()>0){
+
+this.moveVector.normalize()
+
+}
+
+let speed=this.moveSpeed
+
+if(this.keys['ShiftRight']){
+
+speed*=this.sprintMultiplier
+
+}
 
 const accel=this.acceleration*delta
 
-this.velocity.addScaledVector(moveVec,accel*this.moveSpeed)
+vel.addScaledVector(this.moveVector,accel*speed)
 
 }else{
 
 const decay=Math.exp(-this.deceleration*delta)
 
-this.velocity.multiplyScalar(decay)
+vel.multiplyScalar(decay)
 
 }
 
-const speed=this.velocity.length()
+const max=this.maxVelocity
 
-if(speed>this.maxVelocity){
+const lenSq=vel.lengthSq()
 
-this.velocity.multiplyScalar(this.maxVelocity/speed)
+if(lenSq>max*max){
+
+vel.multiplyScalar(max/Math.sqrt(lenSq))
+
+}
+
+}
+
+_computeMovementBasis(){
+
+if(this.space==='world'){
+
+this.forward.set(0,0,-1)
+this.right.set(1,0,0)
+
+return
+
+}
+
+if(this.space==='camera'){
+
+this.camera.getWorldDirection(this.forward)
+
+this.forward.normalize()
+
+this.right.crossVectors(this.forward,this.up).normalize()
+
+return
+
+}
+
+if(this.space==='local'){
+
+this.forward.set(0,0,-1).applyQuaternion(this.camera.quaternion)
+this.right.set(1,0,0).applyQuaternion(this.camera.quaternion)
+
+return
 
 }
 
@@ -128,11 +190,25 @@ setEnabled(enabled){
 
 this.enabled=enabled
 
+this.state=enabled?CONTROLLER_STATE.ACTIVE:CONTROLLER_STATE.DISABLED
+
 }
 
 setSpeed(speed){
 
 this.moveSpeed=speed
+
+}
+
+setSprintMultiplier(multiplier){
+
+this.sprintMultiplier=multiplier
+
+}
+
+setSpace(space){
+
+this.space=space
 
 }
 
@@ -142,12 +218,17 @@ this.velocity.set(0,0,0)
 
 }
 
+getVelocity(){
+
+return this.velocity
+
+}
+
 dispose(){
 
 if(this.disposed)return
 
 window.removeEventListener('keydown',this._keydownHandler)
-
 window.removeEventListener('keyup',this._keyupHandler)
 
 this.velocity=null
@@ -155,6 +236,7 @@ this.direction=null
 this.forward=null
 this.right=null
 this.up=null
+this.moveVector=null
 
 this.camera=null
 
@@ -162,7 +244,7 @@ this.keys=null
 
 this.disposed=true
 
-this.state='disposed'
+this.state=CONTROLLER_STATE.DISPOSED
 
 }
 
